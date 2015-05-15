67c12ec73f3a1de1ca18dbb8ae33766bd8a8e597
Minh Tran <max.thien.tran@gmail.com>
2015-05-14 20:11:44 -0700

    added functions new game, reset all, and update score

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -7,63 +7,101 @@ function tictactoeController(){
     var self = this;
     self.playerOneScore = 0;
     self.playerTwoScore = 0;
-    self.message = "Let's the GAME of TTT begin!";
-    self.messageTaken = "Hey I am taken!";
-    self.messageOWin = "O Win!";
-    self.messageXWin = "X Win!";
-    self.toggle = true; //true will set text to X, false to O
-    self.count = 0; //count number of click
+    self.drawScore = 0;
+    self.message = "Let the GAME of TTT begin!";
+    //true will set text to X, false to O
+    self.toggle = true;
+    //count number of click
+    self.count = 0;
 
-    //getting array of object
+    //setting up 3x3 boxes
     self.square = [
 
-    {text: "" , throne: true} , {text: "", throne: true} , {text: "", throne: true},
-    {text: "" , throne: true} , {text: "", throne: true} , {text: "", throne: true},
-    {text: "" , throne: true} , {text: "", throne: true} , {text: "", throne: true},
+    {text: "" , stark: false, lannister: false} , {text: "", stark: false, lannister: false} , {text: "", stark: false, lannister: false},
+    {text: "" , stark: false, lannister: false} , {text: "", stark: false, lannister: false} , {text: "", stark: false, lannister: false},
+    {text: "" , stark: false, lannister: false} , {text: "", stark: false, lannister: false} , {text: "", stark: false, lannister: false},
 
     ];
 
+    //alternate move between X and O
     self.placeMove = function(i){
-        if(self.square[i].text !== " "){
-            sef.message = self.messageTaken;
-        }else if(self.toggle === true) {
-            self.square[i].text = "X";
-            self.toggle = false;
-        }else if(self.toggle === false){
-            self.square[i].text = "O";
-            self.toggle = true;
+        console.log(self.message);
+        if(self.message == "Let the GAME of TTT begin!" ||
+           self.message == "Hey I am taken!"){
+            if(self.square[i].text !== ""){
+                self.message = "Hey I am taken!";
+            }else if(self.toggle === true) {
+                self.square[i].text = "X";
+                self.square[i].stark = true;
+                self.toggle = false;
+                self.count++;
+            }else if(self.toggle === false){
+                self.square[i].text = "O";
+                self.square[i].lannister = true;
+                self.toggle = true;
+                self.count++;
+            }
+            if (self.count > 4){
+                self.initCheckWinner();
+            }
         }
-        self.initCheckWinner();
 
     };
 
+    /*running to see if there is a winner, if there is passage
+    in message accordingly */
     self.initCheckWinner = function(){
 
         if(self.checkWinner( "X" )){
-            self.message = self.messageXWin;
+            self.message = "House of Stark win!";
+            self.playerOneScore++;
         }else if(self.checkWinner("O")){
-            self.message = self.messageOWin;
+            self.message = "House of Lannister win!";
+            self.playerTwoScore++;
+        }else if(self.count === 9){
+            self.message = "Draw Game!";
+            self.drawScore++;
+        }
+
+    };
+
+    //clear all squares, game start over!
+    self.newGame = function(){
+
+        for(var j= 0; j < self.square.length; j++){
+            self.square[j].text ="";
         }
     };
 
+    self.resetAll = function(){
 
+        self.playerOneScore = 0;
+        self.playerTwoScore = 0;
+        self.newGame();
+
+    };
+
+
+    /*check for winning combinations. verticle connects (3 possible)
+    diagnal (2 possible) and horizontal (3 possible) There are 8 possible
+    way to win*/
     self.checkWinner =function(move){
 
         var result = false;
         if (self.checkRow (0 ,1, 2, move) ||
-        self.checkRow (3, 4, 5, move) ||
-        self.checkRow (6, 7, 8, move) ||
-        self.checkRow (0, 3, 6, move) ||
-        self.checkRow (1, 4, 7, move) ||
-        self.checkRow (2, 5, 8, move) ||
-        self.checkRow (0,  4, 8, move) ||
-        self.checkRow (2, 4, 6, move) ){
-        result = true;
+            self.checkRow (3, 4, 5, move) ||
+            self.checkRow (6, 7, 8, move) ||
+            self.checkRow (0, 3, 6, move) ||
+            self.checkRow (1, 4, 7, move) ||
+            self.checkRow (2, 5, 8, move) ||
+            self.checkRow (0,  4, 8, move) ||
+            self.checkRow (2, 4, 6, move) ){
+                result = true;
         }
         return result;
 
     };
-
+    //compare indext a, b, c, move are equal this is true
     self.checkRow= function(a, b, c, move) {
 
         var result = false;
@@ -78,6 +116,7 @@ function tictactoeController(){
 
     };
 
+    //return the index number from array
     self.getBox = function(i) {
         return self.square[i].text;
     };
